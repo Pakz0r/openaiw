741790568d43fe866e993c026675759261dd0018
Francesco Lombardi <francesco.lombardi1@studenti.uniparthenope.it>
2025-03-11 19:13:12 +0100

    perf(scripts): update run container with shared output dir creation;

--- scripts/run_container.sh
+++ scripts/run_container.sh
@@ -3,9 +3,17 @@
 # Abilita l'uscita immediata in caso di errore
 set -e
 
+# Configura la directory di output dell'applicazione
+PROJECT_DIR=$(pwd)
+OUTPUT_DIR="$PROJECT_DIR/output"
+
+# Crea la directory di output se non esiste
+mkdir -p "$OUTPUT_DIR"
+
 # Avvia il nuovo container con la build
 docker run --rm -it \
     -v /dev:/dev \
+    -v "$OUTPUT_DIR:/app/output" \
     --device-cgroup-rule "c 81:* rmw" \
     --device-cgroup-rule "c 189:* rmw" \
     openaiw
